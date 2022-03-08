df740d9f3dafe9d378a276ef742fd4d873cf2ed7
desaianiruddh <aniruddh.d@simformsolutions.com>
2022-03-08 14:29:36 +0530

    Fixed all the correction

--- todo-react-app/src/components/AddNewTask.jsx
+++ todo-react-app/src/components/AddNewTask.jsx
@@ -3,21 +3,20 @@ import React, { useState } from 'react'
 import checkedImg from '../icon/checked.jpg'
 
 const AddNewTask = (props) => {
+  const {taskName} = props
   const [isTaskCompleted, setIsTaskCompleted] = useState(false);
   return (
     <div className="task d-flex justify-content-between">
-      <div className={isTaskCompleted ? 'completed-task' : 'task-name'}>{props.taskName}</div>
+      <div className={isTaskCompleted ? 'completed-task' : 'task-name'}>{taskName}</div>
       {
-        isTaskCompleted &&
+        isTaskCompleted === true ?
         <img
           src={checkedImg}
           alt="radio-icon"
           className='checked-img'
           onClick={() => setIsTaskCompleted(false)}
         />
-      }
-      {
-        !isTaskCompleted &&
+        :
         <input
           type="radio"
           defaultChecked={false}
