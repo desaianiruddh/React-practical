96a2111515831c9ea1b05825134f8ed08b3a500c
desaianiruddh <aniruddh.d@simformsolutions.com>
2022-03-09 09:32:34 +0530

    resolve conflict

--- practical-2/server.js
+++
@@ -1,8 +0,0 @@
-
-const express=require('express');
-const app=express();
-const port = 5000;
-//run project from dist
-app.use(express.static('dist'));
-//get request
-app.listen(port,()=>console.log(`Task Running on port ${port}`));
